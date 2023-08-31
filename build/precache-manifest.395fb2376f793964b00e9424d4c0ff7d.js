1f955fbb514269a6fcfca080fe653cfd71c35bcf
deepa3321 <142871828+deepa3321@users.noreply.github.com>
2023-08-31 11:50:40 +0530

    Add files via upload

---
+++ build/precache-manifest.395fb2376f793964b00e9424d4c0ff7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b3ba9f622b8dbf151f2e96fc4f0977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d231df0843fe7a8f793d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "41ecc7e266b660674dbf",
+    "url": "/static/js/2.3850e073.chunk.js"
+  },
+  {
+    "revision": "db185181e97e095211720d876bff5692",
+    "url": "/static/js/2.3850e073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d231df0843fe7a8f793d",
+    "url": "/static/js/main.72301921.chunk.js"
+  },
+  {
+    "revision": "0619094419dd4c66ee62",
+    "url": "/static/js/runtime-main.4fc5612e.js"
+  }
+]);
